c4190257a013ee7871e7bcd7e00ef4a6ceb9c357
gunashree-09 <gunashreehk2@gmail.com>
2026-07-23 11:23:33 +0530

    Time: 0 ms (100%), Space: 31.2 MB (95.79%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,16 +11,27 @@
  */
 class Solution {
 public:
-    int c=0;
-    int countNodes(TreeNode* root) {
-        if(root==NULL) return 0;
-
-        countNodes(root->left);
-        c++;
-        countNodes(root->right);
-
-        return c;
-
-        
+    int countNodes(TreeNode* root){
+        if(root == nullptr) return 0;
+        int lh = findLeftHeight(root);
+        int rh = findRightHeight(root);
+        if(lh == rh) return (1<<lh)-1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+    int findLeftHeight(TreeNode* node){
+        int height = 0;
+        while(node){
+            height++;
+            node = node->left;
+        }
+        return height;
+    }
+    int findRightHeight(TreeNode* node){
+        int height = 0;
+        while(node){
+            height++;
+            node = node->right;
+        }
+        return height;
     }
 };
