daa2ddec47ff2cef2136883402da38d875b44799
gunashree-09 <gunashreehk2@gmail.com>
2026-07-27 12:36:14 +0530

    Time: 2 ms (96%), Space: 44.8 MB (78.81%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int l=0;
+        int r=0;
+        int cnt=0;
+        if(s.size()==0) return 0;
+        while(r<s.size() && l<g.size()){
+            if(s[r]>=g[l]){
+                cnt++;
+                l++;
+                r++;
+            }
+            else{
+                r++;
+            }
+        }
+        return cnt;
+    }
+};
