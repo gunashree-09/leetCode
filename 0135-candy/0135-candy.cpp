b0d24e07ffe262e28adeb16b8f5c31cc88dec034
gunashree-09 <gunashreehk2@gmail.com>
2026-07-29 14:06:35 +0530

    Time: 0 ms (100%), Space: 23 MB (52.54%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -10,11 +10,13 @@ public:
             }
             
         }
+        int sum=can[n-1];
         for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]){
                 can[i]=max(can[i],can[i+1]+1);
             }
+            sum+=can[i];
         }
-        return accumulate(can.begin(),can.end(),0);
+        return sum;
     }
 };
